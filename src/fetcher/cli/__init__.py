f81e36a4c60240716ee4835bff1469ef44e83f66
sneakypete81 <sneakypete81@gmail.com>
2023-06-04 12:08:43 +0100

    Add logging

--- src/fetcher/cli/__init__.py
+++ src/fetcher/cli/__init__.py
@@ -9,9 +9,11 @@ from fetcher.__about__ import __version__
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
 @click.argument("url")
+@click.option("-v", "--verbose", is_flag=True)
 @click.version_option(version=__version__)
-def fetcher(url: str) -> int:
-    logging.basicConfig(level=logging.INFO)
+def fetcher(url: str, verbose: bool) -> int:
+    _configure_logging(verbose)
+
     response = asyncio.run(fetch(url))
 
     if not response.ok:
@@ -19,3 +21,16 @@ def fetcher(url: str) -> int:
         raise click.ClickException(msg)
 
     click.echo("Ok")
+
+
+def _configure_logging(verbose: bool):
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(message)s"))
+    logger = logging.getLogger("fetch")
+    logger.addHandler(handler)
+    logger.propagate = False
--- src/fetcher/fetch.py
+++ src/fetcher/fetch.py
@@ -1,14 +1,29 @@
 import asyncio
+import logging
+from typing import Union
 
 from fetcher.http import Request, Response, parse_response
+
+logger = logging.getLogger("fetch")
 
 
 async def fetch(resource: str) -> Response:
     request = Request(resource)
     reader, writer = await asyncio.open_connection(request.hostname, request.port)
+    trace("*", f"Connected to {request.hostname} port {request.port}")
 
+    trace(">", request.data)
     writer.write(request.data)
     writer.write_eof()
     data = await reader.read()
+    trace("<", data)
 
     return parse_response(data)
+
+
+def trace(prefix: str, data: Union[str, bytes]):
+    if isinstance(data, bytes):
+        data = data.decode("ascii")
+
+    for line in data.splitlines():
+        logger.info(f"{prefix} {line}")

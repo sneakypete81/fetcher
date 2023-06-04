eb5b11a9a8a1de1f80ba76f4b8f54f45dbda5a4d
sneakypete81 <sneakypete81@gmail.com>
2023-06-04 07:02:43 +0100

    Set server data in the test bosy

--- tests/test_fetch_integration.py
+++ tests/test_fetch_integration.py
@@ -5,26 +5,31 @@ import pytest
 import fetcher
 
 
-class TestServer:
+class FakeServer:
+    def __init__(self):
+        self.data = b""
+        self._server = None
+
     async def start(self) -> None:
         self._server = await asyncio.start_server(self._on_connect, host="localhost")
 
     def port(self) -> int:
         return self._server.sockets[0].getsockname()[1]
 
-    async def _on_connect(self, reader, writer):
-        data = await reader.read(100)
-        writer.write(data)
+    async def _on_connect(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        await reader.read(1024)
+        writer.write(self.data)
         writer.close()
 
 
 @pytest.fixture(scope="module")
 def server():
-    return TestServer()
+    return FakeServer()
 
 
 @pytest.mark.asyncio
-async def test_fetch_http_url(server):
+async def test_fetch_http_url(server: FakeServer):
+    server.data = b"Hello"
     await server.start()
 
     await fetcher.fetch(f"http://localhost:{server.port()}")

c7fe4c5842d8f2da753898e7be45ccf9cfd290ca
sneakypete81 <sneakypete81@gmail.com>
2023-06-04 09:33:30 +0100

    Add CLI to fetch from a URL

--- src/fetcher/cli/__init__.py
+++ src/fetcher/cli/__init__.py
@@ -1,12 +1,21 @@
+import asyncio
 import logging
 
 import click
 
+from fetcher import fetch
 from fetcher.__about__ import __version__
 
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
+@click.argument("url")
 @click.version_option(version=__version__)
-def fetcher() -> None:
+def fetcher(url: str) -> int:
     logging.basicConfig(level=logging.INFO)
-    click.echo("Hello world!")
+    response = asyncio.run(fetch(url))
+
+    if not response.ok:
+        msg = f"Server returned status '{response.status} {response.status_text}'"
+        raise click.ClickException(msg)
+
+    click.echo("Ok")
